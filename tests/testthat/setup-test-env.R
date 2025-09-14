f2c8dfd2d2ba85a733384359f1091ff9925c7fe1
benjaminhlina <benjamin.hlina@gmail.com>
2025-09-13 20:53:24 -0400

    Change setup file to use new snake_case argument names

--- tests/testthat/setup-test-env.R
+++ tests/testthat/setup-test-env.R
@@ -1,58 +1,60 @@
-nsentinal <- 1
+n_sentinal <- 1
 # ----- standata 1 ------
 standata <- list(
-  nind = model_param_ex$nind, # number of individuals
-  nrec = model_param_ex$nrec, # number of receivers
-  ntime = model_param_ex$tsteps, # number of time steps
-  ntrans = model_param_ex$ntrans, # number of expected transmissions per tag per time interval
-  y = Y, # array of detections
-  recX = rlocs$east, # E-W receiver coordinates
-  recY = rlocs$north, # N-S receiver coordinates
-  xlim = example_extent$xlim, # E-W boundary of spatial extent (receiver array + buffer)
-  ylim = example_extent$ylim
+  n_ind = model_param_ex$n_ind, # number of individuals
+  n_rec = model_param_ex$n_rec, # number of receivers
+  n_time = model_param_ex$tsteps, # number of time steps
+  n_trans = model_param_ex$n_trans, # number of expected transmissions per tag per time interval
+  det = Y, # array of detections
+  rec_x = rlocs$east, # E-W receiver coordinates
+  rec_y = rlocs$north, # N-S receiver coordinates
+  x_lim = example_extent$x_lim, # E-W boundary of spatial extent (receiver array + buffer)
+  y_lim = example_extent$y_lim
 )
 standata_1 <- list(
-  nind = model_param_ex$nind, # number of individuals
-  nrec = model_param_ex$nrec, # number of receivers
-  ntime = model_param_ex$tsteps, # number of time steps
-  ntrans = model_param_ex$ntrans, # number of expected transmissions per tag per time interval
-  y = Y, # array of detections
-  recX = rlocs$east, # E-W receiver coordinates
-  recY = rlocs$north, # N-S receiver coordinates
-  xlim = example_extent$xlim, # E-W boundary of spatial extent (receiver array + buffer)
-  ylim = example_extent$ylim,
-  ntest = nsentinal,
+  n_ind = model_param_ex$n_ind, # number of individuals
+  n_rec = model_param_ex$n_rec, # number of receivers
+  n_time = model_param_ex$tsteps, # number of time steps
+  n_trans = model_param_ex$n_trans, # number of expected transmissions per tag per time interval
+  det = Y, # array of detections
+  rec_x = rlocs$east, # E-W receiver coordinates
+  rec_y = rlocs$north, # N-S receiver coordinates
+  x_lim = example_extent$x_lim, # E-W boundary of spatial extent (receiver array + buffer)
+  y_lim = example_extent$y_lim,
+  n_test = nsentinal,
   test = testY,
-  testX = array(testloc$east, dim = c(nsentinal)),
-  testY = array(testloc$north, dim = c(nsentinal))# N-S b
+  test_x = array(testloc$east, dim = c(n_sentinal)),
+  test_y = array(testloc$north, dim = c(n_sentinal)) # N-S b
 )
 
 # ----- run each model ------
 # ----- standard coa ------
 model_coa_standard <- do.call(
   COA_Standard,
-  c(standata,
+  c(
+    standata,
     list(
       chains = 2,
       warmup = 1000,
       iter = 2000,
       control = list(adapt_delta = 0.95),
       seed = 4,
-      ndraws = 11
+      n_draws = 11
     )
   )
 )
 # ----- time integrated -----
 model_coa_time_vary <- do.call(
   COA_TimeVarying,
-  c(standata,
+  c(
+    standata,
     list(
       chains = 2,
       warmup = 3000,
       iter = 7000,
       control = list(adapt_delta = 0.95),
       seed = 4,
-      ndraws = 11
+      n_draws = 11
     )
   )
 )
@@ -60,16 +62,15 @@ model_coa_time_vary <- do.call(
 # ----- tag integraged -----
 model_coa_tag_int <- do.call(
   COA_TagInt,
-  c(standata_1,
+  c(
+    standata_1,
     list(
       chains = 2,
       warmup = 4000,
       iter = 8000,
       control = list(adapt_delta = 0.95),
       seed = 4,
-      ndraws = 11
+      n_draws = 11
     )
   )
 )
-
-
