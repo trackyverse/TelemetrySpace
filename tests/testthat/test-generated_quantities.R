ae0345dcd5e8c0cd79b17600837aa12149b0ecb5
benjaminhlina <benjamin.hlina@gmail.com>
2025-09-13 20:40:51 -0400

    Change test-genrated_quanities to match the changes in using snake_case

--- tests/testthat/test-generated_quantities.R
+++ tests/testthat/test-generated_quantities.R
@@ -11,18 +11,18 @@ all_data <- list(
   standata_1
 )
 # set the number of draws to test
-ndraws_test <- 5
+n_draws_test <- 5
 
 
 # ----- create function to loop over for errors -----
-call_generated_quantities  <- function(overrides) {
+call_generated_quantities <- function(overrides) {
   do.call(generated_quantities, modifyList(gq_args, overrides))
 }
 # ----- gq_arguments to check ------
 gq_args <- list(
   model = model_coa_standard$model,
   standata = standata,
-  ndraws = ndraws_test
+  n_draws = n_draws_test
 )
 
 # ----- check if params error properly ------
@@ -33,30 +33,36 @@ params_table <- list(
     regex = "`model` must be a Stan object \\(from rstan or cmdstanr\\)\\."
   ),
   list(
-    param = "ndraws",
+    param = "n_draws",
     bad = list("a", NA, c(1, 2)),
-    regex = "`ndraws` must be a numeric vector that has a length of 1."
+    regex = "`n_draws` must be a numeric vector that has a length of 1."
   )
 )
 
 
 # ---- see if it errors properly -----
 test_that("parameter validation works", {
-
   for (pt in params_table) {
     for (bad_val in pt$bad) {
-      tryCatch({
-        expect_error(
-          call_generated_quantities(setNames(list(bad_val), pt$param)),
-          regexp = pt$regex,
-          label = sprintf("param=%s, bad_val=%s", pt$param, deparse1(bad_val))
-        )
-      },
-      error = function(e) {
-        cat("\n Error for param:", pt$param,
-            " bad_val:", deparse1(bad_val), "\n")
-        stop(e)
-      })
+      tryCatch(
+        {
+          expect_error(
+            call_generated_quantities(setNames(list(bad_val), pt$param)),
+            regexp = pt$regex,
+            label = sprintf("param=%s, bad_val=%s", pt$param, deparse1(bad_val))
+          )
+        },
+        error = function(e) {
+          cat(
+            "\n Error for param:",
+            pt$param,
+            " bad_val:",
+            deparse1(bad_val),
+            "\n"
+          )
+          stop(e)
+        }
+      )
     }
   }
 })
@@ -64,40 +70,38 @@ test_that("parameter validation works", {
 
 # create empty list to dump all gc to check
 
-yreps <- list()
+y_reps <- list()
 # ----- loop over generated quantities -----
 for (i in seq_along(all_models)) {
   # Call your function
-  yreps[[i]] <- generated_quantities(model = all_models[[i]]$model,
-                                     standata = all_data[[i]],
-                                     ndraws = ndraws_test)
+  y_reps[[i]] <- generated_quantities(
+    model = all_models[[i]]$model,
+    standata = all_data[[i]],
+    n_draws = n_draws_test
+  )
 }
 
 # length of  each object returned bs = basic_structure
 bs_returned <- c(1, 1, 2)
 
 # length of yrep give the 5 draws
-length_yrep <- ndraws_test
+length_yrep <- n_draws_test
 
 # -----  checks the structure of the structure of generated_quantites -------
 test_that("generated_quantities returns correct structure", {
-
-  for(s in seq_along(yreps)) {
-
-    bs <- yreps[[s]]
+  for (s in seq_along(y_reps)) {
+    bs <- y_reps[[s]]
 
     expect_type(bs, "list")
     expect_length(bs, bs_returned[s])
 
     for (n in seq_along(bs)) {
-
       post_draws <- bs[[n]]
 
       expect_type(post_draws, "list")
       expect_length(post_draws, length_yrep)
 
       for (h in seq_along(post_draws)) {
-
         one_draw <- post_draws[[h]]
 
         expect_true(is.array(one_draw))
@@ -111,16 +115,13 @@ test_that("generated_quantities returns correct structure", {
       }
     }
   }
-}
-)
+})
 
 
 # do not test actual values as these will change
 test_that("generated_quantities returns correct integer ", {
-
-  for(s in seq_along(yreps)) {
-
-    bs <- yreps[[s]]
+  for (s in seq_along(y_reps)) {
+    bs <- y_reps[[s]]
 
     for (n in seq_along(bs)) {
       post_draws <- bs[[n]]
@@ -128,6 +129,4 @@ test_that("generated_quantities returns correct integer ", {
       expect_type(one_draw, "integer")
     }
   }
-}
-)
-
+})
