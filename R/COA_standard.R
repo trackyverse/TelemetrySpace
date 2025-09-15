1afe2a8507ed4089ef301a9ccf85e2205ea24f15
benjaminhlina <benjamin.hlina@gmail.com>
2025-09-15 10:03:19 -0400

    minor formating

--- R/COA_standard.R
+++ R/COA_standard.R
@@ -67,7 +67,7 @@ COA_Standard <- function(
 
   # Summary statistics and convergence diagnostics
   fit_summary <- rstan::summary(fit_model, pars = c("p0", "sigma"))$summary
-  
+
   # How much time did fitting take?
   fit_time <- sum(print(rstan::get_elapsed_time(fit_model))) / 60
 
