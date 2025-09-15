75e536fab6ef3223238401bf4e8e8b7a62a98f07
benjaminhlina <benjamin.hlina@gmail.com>
2025-09-15 11:49:39 -0400

    change test to expect y_rep not yrep

--- tests/testthat/test-COA_Standard.R
+++ tests/testthat/test-COA_Standard.R
@@ -123,7 +123,7 @@ test_that("check to see if model_coa_standard classes", {
   expect_s3_class(model_coa_standard$all_estimates, "data.frame")
   expect_type(model_coa_standard$summary, "double")
   expect_true(is.matrix(model_coa_standard$summary))
-  expect_true(is.matrix(model_coa_standard$generated_quantities$yrep))
+  expect_true(is.matrix(model_coa_standard$generated_quantities$y_rep))
   expect_type(model_coa_standard$generated_quantities, "list")
   expect_true(is.numeric(model_coa_standard$time))
 })
